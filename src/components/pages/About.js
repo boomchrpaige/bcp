3e63491eb0e0508a8c744927f9a217356af1954a
Paul Fernandez <paul.fernandez@discounttire.com>
2021-07-03 22:14:54 -0400

    Third Build

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import '../../App.css';
 
-export default function About() {
-    return <h1 className='about'>ABOUT</h1>;
+function About() {
+    return (
+        <>
+        <h3 className='about'>ABOUT BCP</h3>
+        <p className="about_text">
+        Experimental electronic artist Boom Christopher Paige knew from a young age that his purpose in life was to create. 
+        He's spent his lifetime honing his own unique sound, and now arriving with his latest offering to the audience
+        aching to be challenged.</p>
+        <p className="about_text">Born on the 4th of July, while the sky was lit up with fireworks (hence the nickname “Boom”), the 
+        Philadelphia-based musician demonstrated an affinity for music from the time he was a child, thanks to 
+        his upbringing in a family that favored the arts. Surrounded by all genres of music, from disco to jazz 
+        to opera, his imagination soared, and his voracious appetite for the expressive medium continued to grow.</p>
+        <p className="about_text">Boom spent the majority of the ‘90s as a core member of the electro-punk band <a href="https://www.westword.com/music/burn-baby-burn-5058028" target="_blank" rel="noreferrer">Society Burning</a>, crafting 
+        guitar and keyboard-heavy tracks that were driven by intense drops, but just as he started to find his way 
+        in the music industry, his life was turned upside down when he was diagnosed with Non-Hodgkins Lymphoma 
+        in the mid-90s. However, rather than letting his diagnosis hinder his aspirations, he continued to move 
+        forward, remixing for a handful of bands in the dark electronic world. These experiences sparked a fire 
+        within him to pursue a career as a solo artist, and in 2013, he signed with the New York-based label 
+        Teknofonic Recordings, releasing a handful of experimental singles and EPs, including his 2015 album 
+        “<a href="https://www.beatport.com/release/house-for-serial-killers/1649224" target="_blank" rel="noreferrer">House for Serial Killers</a>.”</p>
+        <p className="about_text">Gathering inspiration from artists like Duran Duran and Imogen Heap, Boom’s sound has been likened to 
+        musicians like Daft Punk, Plaid, and Boards of Canada. In a testament to his nickname, his tracks explode with riveting 
+        energy, bending and breaking the rules through intentional arrangement choices, making use of random 
+        notes and off-kilter rhythms to create ethereal soundscapes that leave the listener wondering what to 
+        expect next. “I make music for people that love music beyond the social aspects,” he explains, “who 
+        insert themselves into the songs they hear.”</p>
+        <p className="about_text">Boom has released his latest album “<a href="https://boomchrpaige.bandcamp.com/album/membranes" target="_blank" rel="noreferrer">Membranes</a>,” which explores a non-musical concept—the COVID-19 
+        pandemic—in an unconventional way. Designed specifically for presentation on vinyl, the album consists 
+        of two sides, with the first conveying the uncertainty and naive optimism the world experienced during 
+        the beginning of the crisis, and the second side providing a darker contrast, instead focusing on the 
+        trauma and brokenness that reverberated internationally as the pandemic worsened and impacted daily life.</p>
+
+
+        </>
+
+    );
 }
+
+export default About;

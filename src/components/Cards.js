4ffe3d55948ba259d1f903dbf6c70dfb5afe8dc4
Paul Fernandez <paul.fernandez@discounttire.com>
2021-07-05 11:52:02 -0400

    8.1

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,13 +8,13 @@ function Cards() {
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                 <ul className='cards__items'>
-                <li className="playlist"><a href='https://open.spotify.com/playlist/2CsTZ92uYaXI7oYuoq7NwM?si=542b93cc4de64f2c&nd=1' target='_blank' rel='noreferrer'>Above London featured on Music Enthusiast's New Music Friday playlist, July 2021</a></li>
-                <li className="interview"><a href='https://bit.ly/3quH2ne' target='_blank' rel='noreferrer'>Interview with Mundane Magazine, June 2021</a></li>
-                <li className="review"><a href='https://bit.ly/3wXMxgE' target='_blank' rel='noreferrer'>Membranes LP review from Exclusive Magazine, June 2021</a></li>
-                <li className="interview"><a href='https://bit.ly/3iBMSRL' target='_blank' rel='noreferrer'>Boom chr Paige Interview with Music Existence, June 2021</a></li>
-                <li className="interview"><a href='https://bit.ly/3uF0s9W' target='_blank' rel='noreferrer'>Boom chr Paige Interview with Vents Magazine, May 2021</a></li>
-                <li className="feature"><a href='https://bit.ly/2R3bt6m' target='_blank' rel='noreferrer'>Boom chr Paige Feature on Stitched Sound, May 2021</a></li>
-                <li className="feature"><a href='https://bit.ly/3ta5p9j' target='_blank' rel='noreferrer'>Above London Feature on the Tinnitist Blog, May 2021</a></li>
+                <li className="playlist"><a href='https://open.spotify.com/playlist/2CsTZ92uYaXI7oYuoq7NwM?si=542b93cc4de64f2c&nd=1' target='_blank' rel='noreferrer'>Feature @ Music Enthusiast's New Music Friday, July</a></li>
+                <li className="interview"><a href='https://bit.ly/3quH2ne' target='_blank' rel='noreferrer'>Interview with Mundane Magazine, June</a></li>
+                <li className="review"><a href='https://bit.ly/3wXMxgE' target='_blank' rel='noreferrer'>Membranes LP review from Exclusive Magazine, June</a></li>
+                <li className="interview"><a href='https://bit.ly/3iBMSRL' target='_blank' rel='noreferrer'>Boom chr Paige Interview with Music Existence, June</a></li>
+                <li className="interview"><a href='https://bit.ly/3uF0s9W' target='_blank' rel='noreferrer'>Boom chr Paige Interview with Vents Magazine, May</a></li>
+                <li className="feature"><a href='https://bit.ly/2R3bt6m' target='_blank' rel='noreferrer'>Boom chr Paige Feature on Stitched Sound, May</a></li>
+                <li className="feature"><a href='https://bit.ly/3ta5p9j' target='_blank' rel='noreferrer'>Above London Feature on the Tinnitist Blog, May</a></li>
                 </ul>
                 </div>
             </div>
